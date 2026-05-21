85ffceaac7220cfaa1b5fa1ed10ae6de6c99e2e9
SyFord421 <jaki05118@gmail.com>
2026-05-21 18:35:49 +0700

    fix: do a major refactoring(not finished yet)

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,91 @@
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, WriteHalf};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
-use tokio::sync::Mutex;
-use std::sync::Arc;
+use tokio::sync::broadcast;
 use std::process;
 
-struct Client {
-    username: String,// nama user
-    address: String,// alamat port
-    writer: WriteHalf<TcpStream>// Writer milik user
-}
 
-// 1. Untuk mengurus 1 User di awal
-// Fungsi untuk handle Pesan User
-async fn handle_request(socket: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
+// Data custom biar nggak kepanjangan nulis di fungsin
+type SenderChannel = broadcast::Sender<(String, String)>;
+type ReceiverChannel = broadcast::Receiver<(String, String)>;
 
-// Ambil alamat IP & Port unik si user sebagai KTP
-    let client_addr = socket.peer_addr().unwrap().to_string(); 
-    
-// belah socket jadi 2 bagian
-    let(reader, mut writer) = tokio::io::split(socket);
-// Shadowing biar bisa baca input user
-    let mut reader = BufReader::new(reader);
-    
-// minta input username
-    let mut name = String::new();
-
-    let _ = writer.write_all(b"Siapa Namamu Tuan?: ").await;
-    
-// flush biar nggak nyangkut di buffer
-    let _ = writer.flush().await;
-    let _ = reader.read_line(&mut name).await;
-    let username = name.trim().to_string();
-    println!("[Log] {} Telah bergabung dari {}", username, client_addr);
-    
-    {// delimiter agar kunci bisa di buka
-    
-// kunci daftar agar tidak ada rebutan (.await menunggu giliran input user)
-        let mut list = clients.lock().await;
-        let new_client = Client {
-            username: username,
-            address: client_addr.clone(),
-            writer: writer
-        };
-        list.push(new_client);
-    }// <- Kunci Otomatis akan di buka di sini
-    
-    loop {
-    let mut msg = String::new();
-    match reader.read_line(&mut msg).await {
-        Ok(0) => {
-            break;
-        },
-        Ok(_) => {
-            // kunci semuanya sementara karena akan melakukan siaran 
-            let mut list = clients.lock().await;
-            // lakukan perulangan untuk mengirimkan pesan ke semua orang 
-            for client in list.iter_mut() {
-                let broadcast = format!("\x1B[1A\x1B[2K[{}]: {}\n", client.username, msg.trim());
-                let _ = client.writer.write_all(broadcast.as_bytes()).await;
-                let _ = client.writer.flush().await;
-                println!("[Log]| {}", broadcast);
-            }
-        },
-        Err(_) => break,
-    }
-} 
-{
-// Fungsi untuk menghapus User saat keluar
-    let mut list = clients.lock().await;
-    list.retain(|client| client.address != client_addr);
-}// Buka Lock di sini
-}
 
 #[tokio::main]
-async fn main() {
-// Buat daftar buku bersama (Arc) dan 1 alat tulis untuk di oper bergantian (Mutex)
-    let clients: Arc<Mutex<Vec<Client>>> = Arc::new(Mutex::new(Vec::new()));
-// Siapkan Jalur Komunikasi (Port)
-    let listener = tokio::net:: TcpListener::bind("127.0.0.1:8080").await.unwrap_or_else( |err| {
+async fn  main() {
+    let listener = tokio::net::TcpListener:: bind("127.0.0.1:8080")
+    .await
+    .unwrap_or_else(|err|{
         println!("{}", err);
         process::exit(1)
     });
-
-    println!("[OK] Server Is Running...");
+    // Buat Channel Broadcast utama
+    // 16 adalah panjang karakter pesan
+    let (tx, _) = broadcast::channel::<(String, String)>(16); 
+    println!("[Ok] Server Is Running...");
     
+    // Tugas main hanyalah menjadi pendengar
     loop {
-        if let Ok((socket, _ )) = listener.accept().await {
-        let client_clone = Arc::clone(&clients);
-        tokio::spawn(async move {
-            handle_request(socket, client_clone).await;
-        });
-        }else{
-            println!("[Warning] Gagal menerima tamu baru, mencoba lagi...");
+        if let Ok((socket, _)) = listener.accept().await {
+            let tx_clone = tx.clone();
+            let rx_clone = tx.subscribe();
+            tokio::spawn(async move{
+                handle_users(socket, tx_clone, rx_clone)
+            }.await;);
         }
+    }
+}
+
+
+// Fungsi untuk mengurusi User Dari awal Sampe bosen
+async fn handle_users(socket: TcpStream, tx: SenderChannel, mut rx: ReceiverChannel) { // nah tipe data custom tadi di gunakan di sini kalau nggak gitu nantinya gini  tx: broadcast::Sender<(String, String)>
+    let client_addr = socket.peer_addr(). unwrap().to_string();
+    // belah kabel Jadi 2 bagian satu untukmu satu untuku hehe
+    let (reader, mut writer) = tokio::io::split(socket);
+    // Shadowing biar simple dan biar nggak buta huruf
+    let mut reader = BufReader::new(reader);
+    // proses perkenalan pake nama asli yah jangan pake nama politik
+    let mut name = String::new();
+    let _ = writer.write_all(b"Siapa Namamu Tuan?\n").await;//namaku dizz
+    // Kirim paksa biar nggak di timbun
+    let _ = writer.flush().await;
+    if reader.read_line(&mut name).await.is_err() {return;}// kalo gagal baca minta input lagi dengan return ke awal
+    let username= name.trim().to_string();
+    
+    println!("{} Telah Bergabung Dari Port {}", username, client_addr);
+    // Umumkan Tamu baru biar ketahuan
+    let _ = tx.send(("Server".to_string(), format!("{} Telah Bergabung..", username)));
+
+    let mut msg = String::new();
+    loop {
+        tokio::select! {
+            // dengerin ketikan user 
+            res = reader.read_line(&mut msg) => {
+                match res {
+                    Ok(0) => break,
+                    Ok(_) => {
+                        let _ = tx.send((username.clone(), msg.trim().to_string()));
+                        msg.clear(); 
+                    }
+                    Err(_) => break,
+                }
+            }
+            result = rx.recv() => {
+                if let Ok(())
+            }
+        }
+    }
+}
+    // Proses Keluar
+    println!("[Log] {} Keluar.", username);
+    let _ = tx.send(("Server".to_string(), format!("{} meninggalkan obrolan.", username)));
+
+}
+
+fn format_message(sender: &str, username: &str, txt: &str) -> String{
+    if sender == username {
+        format!("\x1B[1A\x1B[2K[Anda]: {}\n", txt)
+    } else if sender == "Server"{
+            format!("[INFO]: {}\n", txt)
+        } else {
+        format!("[{}]: {}\n", sender, txt)
     }
 }
